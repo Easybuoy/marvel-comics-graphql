e71154b047c8af2977c4eaef6819e8eb2fa613f5
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-09-14 16:13:20 +0100

    edit readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # marvel-comics-graphql
 Web Application that displays information about Marvel Comics and Characters.
+
+This project was created to demonstrating how to get started with React & Apollo Client
+
+Link to article: 
+
+Link to GraphQL Api used: https://github.com/Novvum/MarvelQL
+
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import CharacterWithHOC from "./CharacterWithHOC";
-import CharacterWithHook from './CharacterWithHook';
+// import CharacterWithHOC from "./CharacterWithHOC";
+// import CharacterWithHook from './CharacterWithHook'; 
 import CharacterWithRender from './CharacterWithRender';
 
 function App() {
