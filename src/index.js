6ea25558e3782943e6e281daec148ae9a1309a32
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-09-15 21:24:02 +0100

    edit index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
-// import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
